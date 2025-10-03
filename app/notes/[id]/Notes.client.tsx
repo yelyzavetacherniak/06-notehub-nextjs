eff3f53fa81dc8d777adde33d628247849a11ba3
yelyzaveta_cherniak <lcherniak5gmail.com>
2025-10-04 00:12:53 +0200

    add changes

--- app/notes/[id]/Notes.client.tsx
+++ app/notes/[id]/Notes.client.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import css from './NoteDetails.module.css';
+import css from './Notes.client.module.css';
 
 import { useQuery } from '@tanstack/react-query';
 import { fetchNoteById } from '../../../lib/api';
